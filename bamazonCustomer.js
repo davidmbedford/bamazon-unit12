86c7de307803a562b5f48c92d08e55a1a1506cc9
DavidmB <david.m.bedford1@gmail.com>
2019-05-09 13:39:55 -0400

    set up inquirer, getting all needed info

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,9 +1,7 @@
 const inquirer = require('inquirer');
 const mysql = require('mysql');
 
-//list inventory on init: id, name, price
-
-var connection = mysql.createConnection({
+let connection = mysql.createConnection({
   host: "localhost",
 
   port: 3306,
@@ -21,32 +19,38 @@ connection.connect(function(err) {
 });
 
 function beginApp() {
-let query = "SELECT id, product_name, price FROM products";
-console.log(query);
+let query = "SELECT id, product_name, price, stock_quantity  FROM products";
   connection.query(query, function(err, res) {
-    console.log(res);
     for (var i = 0; i < res.length; i++) {
-      console.log("ID: " + res[i].id + " || Product: " + res[i].product_name + " || Price: " + res[i].price);
+      console.log("ID: " + res[i].id + " || Product: " + res[i].product_name + " || Price: " + res[i].price + " || In-Stock: " + res[i].stock_quantity);
       };
+
+      inquirer
+      .prompt ([
+        {
+          name: "item",
+          type: "input",
+          message: "What item would you like to purchase? Enter the ID number here (only one product at a time): "
+        },
+        {
+          name: "quantity",
+          type: "input",
+          message: "How many would you like? "
+        }
+      ])
+      .then(function(response) {
+        console.log(response.item);
+        console.log(response.quantity);
+        let inStock = res[response.item - 1].stock_qunatity);
+
+        if (response.quantity > inStock) {
+        console.log("Insuffecient quantity! We only have " + inStock + " in stock. Sorry for the inconvenience, please try again!");
+        beginApp();
+        }
+        //// buy
+        // else
+        //// insuffecient quantity
+        //////try again
+      });
     })
   }
-
-
-// inquirer
-//   .prompt ([
-
-//     {
-//       //ask what they want
-//     },
-//     {
-//       //ask how many they want
-//     }
-//   ])
-//   .then(function(response) {
-//     //if (enough)
-//     //// buy
-//     // else
-//     //// insuffecient quantity
-//     //////try again
-
-//   })
