78cbd54c82f954424c2c06c869398e477abea02a
DavidmB <david.m.bedford1@gmail.com>
2019-05-12 12:36:24 -0400

    3/4 done yesterday, couldnt push, bad wifi

--- bamazonManager.js
+++ bamazonManager.js
@@ -43,8 +43,8 @@ inquirer
     } else if (command === 'Add New Product') {
       addProduct();
     }
-    beginManager();
     });
+  };
 
   const viewInventory = function () {
     console.log('\n=========================================');
@@ -58,6 +58,8 @@ inquirer
         console.log("ID: " + res[i].id + " || Product: " + res[i].product_name + " || Price: " + res[i].price + " || In-Stock: " + res[i].stock_quantity);
       };
       console.log('=========================================\n');
+
+      beginManager();
     })
   };
 
@@ -65,20 +67,73 @@ inquirer
     console.log('\n=========================================');
     console.log('This viewLow  is working!');
     console.log('=========================================\n');
-    //beginManager();
+
+    let query = "SELECT id, product_name, price, stock_quantity FROM products";
+    connection.query(query, function(err, res) {
+      console.log('\n=========================================');
+      for (var i = 0; i < res.length; i++) {
+        if (res[i].stock_quantity <= 5) {
+          console.log("ID: " + res[i].id + " || Product: " + res[i].product_name + " || Price: " + res[i].price + " || In-Stock: " + res[i].stock_quantity);
+        }
+      };
+      console.log('=========================================\n');
+
+      beginManager();
+    })
   };
 
   const addInventory = function () {
     console.log('\n=========================================');
     console.log('This addInventory is working!');
     console.log('=========================================\n');
-    //beginManager();
+
+    let query = "SELECT id, product_name, price, stock_quantity FROM products";
+    connection.query(query, function(err, res) {
+      for (var i = 0; i < res.length; i++) {
+        console.log("ID: " + res[i].id + " || Product: " + res[i].product_name + " || Price: " + res[i].price + " || In-Stock: " + res[i].stock_quantity);
+        };
+
+      console.log('\n===================\n');
+
+    inquirer
+    .prompt ([
+      {
+        name: "item",
+        type: "input",
+        message: "What item would you like to stock up on? Enter ID number: "
+      },
+      {
+        name: "quantity",
+        type: "input",
+        message: "How many would you like to add?"
+      }
+    ])
+    .then(function(response) {
+      product = response.item;
+      userAmt = response.quantity;
+      inStock = res[product - 1].stock_quantity;
+
+      function updateQuantity() {
+          let newAmt = (parseInt(inStock) + parseInt(userAmt));
+          let update = "UPDATE products SET stock_quantity = " + newAmt + " WHERE id = " + (product);
+          connection.query(update, function(err, res) {
+            console.log(newAmt + " left in stock")
+            });
+          console.log('=========================================\n');
+        };
+
+      updateQuantity();
+      beginManager();
+
+      });
+
+    });
   };
+
 
   const addProduct = function () {
     console.log('\n=========================================');
     console.log('This addProduct is working!');
     console.log('=========================================\n');
-    //beginManager();
+    beginManager();
   };
-}
